4140a833ba77667fa89420c9107b98a26102f8bb
Your Name <you@example.com>
2021-04-08 20:16:15 -0400

    Your Message

--- src/components/components/Spin.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import CircularProgress from '@material-ui/core/CircularProgress';
-
-const Spin = ({ size }: { size: number }) => {
-  return <CircularProgress size={size} />;
-};
-
-export default Spin;
